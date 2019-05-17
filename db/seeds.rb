de15c88ed2da9ea6a006a9c2c3c8883c30f20a6f
Jean-Guillaume HERBRETEAU <jgherbreteau@hotmail.com>
2019-05-17 12:41:10 +0200

    hey

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+require 'open-uri'
 
 
-Cocktail.create(name: "Mojito")
-Cocktail.create(name: "Malibu on the Beach")
-Cocktail.create(name: "Blue Mary")
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+parsing = open(url).read
+data = JSON.parse(parsing)
+
+data['drinks'].each do |i|
+  Ingredient.create!(name: i['strIngredient1'])
+end
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: ‘Star Wars’ }, { name: ‘Lord of the Rings’ }])
+#   Character.create(name: ‘Luke’, movie: movies.first)
